651aab0d26e31da7d2dd762f14abea896c6fdb64
NoamRon1 <noam2009r@gmail.com>
2025-04-15 11:39:30 +0000

    unknowen changes

--- src/my_robot_package/my_robot_package/real_button_node.py
+++ src/my_robot_package/my_robot_package/real_button_node.py
@@ -12,7 +12,6 @@ class RealButtonPublisher(Node):
         super().__init__('real_button_publisher')
 
         self.publisher_ = self.create_publisher(Bool, 'button_press', 10)
-        self.buildInLed_ self.
         self.last_state = False
 
         GPIO.setmode(GPIO.BCM)
